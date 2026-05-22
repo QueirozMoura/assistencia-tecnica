b951e05ebc3b56b8ad936371b9a670dc583f01e9
QueirozMoura <gqueiroz2008@gmail.com>
2026-05-22 12:40:37 -0300

    `Update App component to include basename in BrowserRouter`

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './styles/globals.css';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/assistencia-tecnica">
       <AppRoutes />
     </BrowserRouter>
   );
